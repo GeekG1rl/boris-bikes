93ac3d074aa5a084f13d0bcc351da3887b14018f
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-04-15 11:02:55 +0100

    Failing new feature test for reporting as broken

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,8 +4,15 @@ describe Bike do
   it {is_expected.to respond_to(:working?)}
 
 	describe "#report_broken" do
-	    #"allows a user to report a broken bike"
+
 	  it {is_expected.to respond_to(:report_broken)}
+
+    it "allows a user to report a broken bike" do
+      bike = Bike.new
+      bike.report_broken
+      expect(bike.working?).to eq false
+    end
+
 	end
-	
+
 end
